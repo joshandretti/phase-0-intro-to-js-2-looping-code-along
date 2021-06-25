c45ba11b1b6bad6e735919c34cb44579ac61d438
Josh <josh22henry@gmail.com>
2021-06-25 12:45:06 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-// Code your solutions in this file
-
 function writeCards( namesArray, event ) {
-    let thankYouCards = []
-    for ( let i = 0; i < namesArray.length; i++ ) {
-      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
-    }
-    return thankYouCards
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
   }
-  
-  function countDown( startingNumber ) {
-    while ( startingNumber > 0 ) {
-      console.log( startingNumber );
-      startingNumber -= 1;
-    }
+  return thankYouCards
+}
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
     console.log( startingNumber );
+    startingNumber -= 1;
   }
+  console.log( startingNumber );
+}
